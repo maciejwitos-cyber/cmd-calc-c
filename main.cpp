e3f52f629bee43bc32bf7b670c7f601c84fd680c
Maciej Witos <maciej.witos@gmail.com>
2026-09-01 22:21:15 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstring>
+
+void usage(char *programName) {
+    printf("Usage: %s number1 operation number2\n", programName);
+    printf("Available operations:\n");
+    printf("\tadd\n");
+    printf("\tsub\n");
+    printf("\tmul\n");
+    printf("\tdiv\n");
+    printf("\n");
+}
+
+int main(int argc, char* argv[]) {
+
+    // argv[0] - nazwa programu
+    // argv[1] - pierwsza liczba
+    // argv[2] - dzialanie
+    // argv[3] - druga liczba
+
+    int num1 = 0;
+    int num2 = 0;
+    int result = 0;
+
+    if (argc == 4) {
+        num1 = atoi(argv[1]);
+        num2 = atoi(argv[3]);
+
+        if (!strcmp("add", argv[2])) {
+            result = num1 + num2;
+            printf("%d + %d = %d\n", num1, num2, result);
+            return 0;
+        }
+
+        if (!strcmp("sub", argv[2])) {
+            result = num1 - num2;
+            printf("%d - %d = %d\n", num1, num2, result);
+            return 0;
+        }
+
+        if (!strcmp("mul", argv[2])) {
+            result = num1 * num2;
+            printf("%d * %d = %d\n", num1, num2, result);
+            return 0;
+        }
+        if (!strcmp("div", argv[2])) {
+            result = num1 / num2;
+            printf("%d / %d = %d\n", num1, num2, result);
+            return 0;
+        }
+
+    }
+
+    usage(argv[0]);
+
+    return 0;
+}
+
